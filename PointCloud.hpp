c8a4dc5b14b9feab7a389c9e75ad3bc2f99d8252
Alexandre Savard <alexagone@gmail.com>
2020-10-19 21:20:31 -0400

    Add Point class in PointCloud.hpp
    1) Add Point class
    2) Test copy constructor and assignment operator in main.cpp

--- PointCloud.hpp
+++ PointCloud.hpp
@@ -2,6 +2,28 @@
 
 namespace Cloud {
 
+struct Point
+{
+    Point(double x=0.0, double y=0.0) : x(x), y(y) {}
+
+    Point(const Point& p)
+    {
+        x = p.x;
+        y = p.y;
+    }
+
+    Point& operator=(const Point& p)
+    {
+        x = p.x;
+        y = p.y;
+
+        return *this;
+    }
+
+    double x;
+    double y; 
+};
+
 class PointCloud
 {
 };
--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "PointCloud.hpp"
 
+#include <iostream>
+
 using namespace Cloud;
+using namespace std;
 
 int main()
 {
     PointCloud pc = PointCloud();
+
+    Point a{0.0, 2.0};
+    Point b(a);
+    Point c = b;
+    cout << b.x << " " << b.y << endl;
+    cout << c.x << " " << c.y << endl;
 
     return 0;
 }
